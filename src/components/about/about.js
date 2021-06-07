394eaabd8c6b934ede2f57ef9815cbaf4ecdc9c0
Pedro Ricardo <pedro.ricardo123go@gmail.com>
2021-06-07 17:35:26 -0300

    v4

--- src/components/about/about.js
+++ src/components/about/about.js
@@ -4,11 +4,11 @@ import './about.css'
 
 function About() {
   return (
-   <>
+  
    <div className="AboutPage">
     <header className="AboutHeader"><h1></h1></header>
    </div>
-   </>
+   
   );
 }
 
